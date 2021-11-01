284e6a3fc7dfb6709f40b957e69f52c7b0c5ff9b
IkerGalardi <contacto.ikergalardi@gmail.com>
2021-11-01 12:03:51 +0100

    Added more tests

--- test/main.cc
+++ test/main.cc
@@ -24,13 +24,36 @@ void test_vector_addition() {
     vecf<6> a = {1, 2, 3, 1, 2, 3};
     vecf<6> b = {1, 2, 3, 1, 2, 3};
     assert((a + b == vecf<6>{2, 4, 6, 2, 4, 6}));
+
+    vecf<3> c = {1, 2, 3};
+    vecf<3> d = {1, 2, 3};
+    assert((c + d == vecf<3>{2, 4, 6}));
+
+    vecf<9> e = {1, 2, 3, 1, 2, 3, 1, 2, 3};
+    vecf<9> f = {1, 2, 3, 1, 2, 3, 1, 2, 3};
+    assert((e + f == vecf<9>{2, 4, 6, 2, 4, 6, 2, 4, 6}));
+
+    vecf<1> g = {1};
+    vecf<1> h = {1};
+    assert((g + h == vecf<1>{2}));
 }
 
 void test_vector_substraction() {
-    vecf<3> a = {1, 2, 3};
-    vecf<3> b = {1, 2, 3};
-    assert((a - b == vecf<3>{0, 0, 0}));
-    
+    vecf<6> a = {1, 2, 3, 1, 2, 3};
+    vecf<6> b = {1, 2, 3, 1, 2, 3};
+    assert((a - b == vecf<6>{0, 0, 0, 0, 0, 0}));
+
+    vecf<3> c = {1, 2, 3};
+    vecf<3> d = {1, 2, 3};
+    assert((c - d == vecf<3>{0, 0, 0}));
+
+    vecf<9> e = {1, 2, 3, 1, 2, 3, 1, 2, 3};
+    vecf<9> f = {1, 2, 3, 1, 2, 3, 1, 2, 3};
+    assert((e - f == vecf<9>{0, 0, 0, 0, 0, 0, 0, 0, 0}));
+
+    vecf<1> g = {1};
+    vecf<1> h = {1};
+    assert((g - h == vecf<1>{0}));
 }
 
 int main() {
