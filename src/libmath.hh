8cb29706a053d0e7161f995c395f4454971e4bd5
IkerGalardi <contacto.ikergalardi@gmail.com>
2021-11-02 11:46:35 +0100

    Changed to generic SSE instead of SSE2
    Changing SSE2 from more recent versions to use functions like horizontal addition

--- src/libmath.hh
+++ src/libmath.hh
@@ -1,10 +1,10 @@
 #pragma once
 
-#define LIBMATH_BACKEND_SIMD_SSE2
+#define LIBMATH_BACKEND_SIMD_SSE
 
 #if defined(LIBMATH_BACKEND_SIMPLE_CPU)
     #define LIBMATH_ALIGNMENT   
-#elif defined(LIBMATH_BACKEND_SIMD_SSE2)
+#elif defined(LIBMATH_BACKEND_SIMD_SSE)
     #define LIBMATH_ALIGNMENT alignas(16) 
 #endif
 
@@ -13,6 +13,6 @@
 // Include the selected backend implementation
 #if defined(LIBMATH_BACKEND_SIMPLE_CPU)
     #include "backends/simple_cpu.hh"
-#elif defined(LIBMATH_BACKEND_SIMD_SSE2)
-    #include "backends/sse2.hh"
+#elif defined(LIBMATH_BACKEND_SIMD_SSE)
+    #include "backends/sse.hh"
 #endif
