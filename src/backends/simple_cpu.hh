27cdb9844537851fbd33984c26512e674c7636c4
Iker Galardi <contacto.ikergalardi@gmail.com>
2021-11-05 10:13:15 +0100

    Vector: multiplication operations (#4)
    Added multiplication operations for vector*vector and scalar*vector. The operations are implemented in both backends and tested.

--- src/backends/simple_cpu.hh
+++ src/backends/simple_cpu.hh
@@ -10,7 +10,7 @@ template<size_t size>
 float length_squared(const vecf<size>& vec) {
     float sum = 0.0;
     for(int i = 0; i < size; i++) {
-        sum += vec[i] * vec[i];
+        sum += vec.data[i] * vec.data[i];
     }
 
     return sum;
@@ -46,6 +46,32 @@ vecf<size> operator-(const vecf<size>& left, const vecf<size>& right) {
     vecf<size> result;
     for(int i = 0; i < size; i++)
         result.data[i] = left.data[i] - right.data[i];
+
+    return result;
+}
+
+/*
+ * Performs the multiplication between two vectors of equal size. If the sizes of the 
+ * vectors don't match a compilation error is thrown.
+ */
+template<size_t size>
+vecf<size> operator*(const vecf<size>& left, const vecf<size>& right) {
+    vecf<size> result;
+    for(int i = 0; i < size; i++)
+        result.data[i] = left.data[i] * right.data[i];
+
+    return result;
+}
+
+/*
+ * Performs the multiplication between a scalar and a vector of equal size. If the sizes
+ * of the vectors don't match a compilation error is thrown.
+ */
+template<size_t size>
+vecf<size> operator*(float scalar, const vecf<size>& right) {
+    vecf<size> result;
+    for(int i = 0; i < size; i++)
+        result.data[i] = scalar * right.data[i];
 
     return result;
 }
