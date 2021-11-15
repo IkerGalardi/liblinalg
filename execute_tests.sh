a18a3c2ac047d574f424ceac3ec455bfaa8a2581
IkerGalardi <contacto.ikergalardi@gmail.com>
2021-11-15 13:21:04 +0100

    Pretier printing of missmatch test

--- execute_tests.sh
+++ execute_tests.sh
@@ -3,10 +3,12 @@
 # Execute the operations test
 echo "- Operation correctness tests"
 sh test/operations.sh
+echo ""
 
 # Execute vector size missmatch error test
 echo "- Vector size missmatch error test"
 sh test/vector_size_missmatch.sh
 
+echo ""
 echo "- Matrix size missmatch error test"
 sh test/matrix_size_missmatch.sh
--- test/matrix_size_missmatch.sh
+++ test/matrix_size_missmatch.sh
@@ -4,14 +4,8 @@ COMPILATION_FLAGS="-msse4.1 -O2 -m64 -std=c++17"
 INCLUDE_FLAGS="-Isrc"
 
 
-echo "Addition missmatch:---------------------------------------------------------"
-g++ $COMPILATION_FLAGS $INCLUDE_FLAGS -DLIBMATH_BACKEND_SIMPLE_CPU -DMISSMATCH_ADD test/src/matrix_size_missmatch.cc -o bin/matrix_size_missmatch
-echo "----------------------------------------------------------------------------"
+g++ $COMPILATION_FLAGS $INCLUDE_FLAGS -DLIBMATH_BACKEND_SIMPLE_CPU -DMISSMATCH_ADD test/src/matrix_size_missmatch.cc -o bin/matrix_size_missmatch 2> /dev/null || echo "Addition missmatch... OK"
 
-echo "Substraction missmatch:-----------------------------------------------------"
-g++ $COMPILATION_FLAGS $INCLUDE_FLAGS -DLIBMATH_BACKEND_SIMPLE_CPU -DMISSMATCH_SUB test/src/matrix_size_missmatch.cc -o bin/matrix_size_missmatch
-echo "----------------------------------------------------------------------------"
+g++ $COMPILATION_FLAGS $INCLUDE_FLAGS -DLIBMATH_BACKEND_SIMPLE_CPU -DMISSMATCH_SUB test/src/matrix_size_missmatch.cc -o bin/matrix_size_missmatch 2> /dev/null || echo "Substraction missmatch... OK"
 
-echo "Multiplication missmatch:---------------------------------------------------"
-g++ $COMPILATION_FLAGS $INCLUDE_FLAGS -DLIBMATH_BACKEND_SIMPLE_CPU -DMISSMATCH_MUL test/src/matrix_size_missmatch.cc -o bin/matrix_size_missmatch
-echo "----------------------------------------------------------------------------"
+g++ $COMPILATION_FLAGS $INCLUDE_FLAGS -DLIBMATH_BACKEND_SIMPLE_CPU -DMISSMATCH_MUL test/src/matrix_size_missmatch.cc -o bin/matrix_size_missmatch 2> /dev/null || echo "Multiplication missmatch... OK"
