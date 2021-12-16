e084eb94e81d48ec00750e2e69b36cbb57dc27e2
IkerGalardi <contacto.ikergalardi@gmail.com>
2021-12-16 09:44:17 +0100

    Single script with parameters

--- benchmark.sh
+++ benchmark.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-COMPILATION_FLAGS="-msse4.1 -m64 -std=c++17"
+COMPILATION_FLAGS="-msse4.1 -mavx2 -m64 -std=c++17"
 INCLUDE_FLAGS="-Isrc"
 
 echo "- Simple CPU results:"
@@ -17,16 +17,16 @@ g++ $COMPILATION_FLAGS $INCLUDE_FLAGS -DLIBLINALG_BACKEND_SIMPLE_CPU bench/src/m
 g++ $COMPILATION_FLAGS $INCLUDE_FLAGS -DLIBLINALG_BACKEND_SIMPLE_CPU bench/src/matrix_vector_bench.cc -o bin/matrix_vector_bench 2> /dev/null
 ./bin/matrix_vector_bench
 
-echo "- SSE results:"
+echo "- $1 results:"
 
 # Vector benchmark
-g++ $COMPILATION_FLAGS $INCLUDE_FLAGS -DLIBLINALG_BACKEND_SIMD_SSE bench/src/vector_bench.cc -o bin/vector_bench 2> /dev/null
+g++ $COMPILATION_FLAGS $INCLUDE_FLAGS -DLIBLINALG_BACKEND_$1 bench/src/vector_bench.cc -o bin/vector_bench 2> /dev/null
 ./bin/vector_bench
 
 # Matrix benchmark
-g++ $COMPILATION_FLAGS $INCLUDE_FLAGS -DLIBLINALG_BACKEND_SIMD_SSE bench/src/matrix_bench.cc -o bin/matrix_bench 2> /dev/null
+g++ $COMPILATION_FLAGS $INCLUDE_FLAGS -DLIBLINALG_BACKEND_$1 bench/src/matrix_bench.cc -o bin/matrix_bench 2> /dev/null
 ./bin/matrix_bench
 
 # Vector matrix benchmark
-g++ $COMPILATION_FLAGS $INCLUDE_FLAGS -DLIBLINALG_BACKEND_SIMD_SSE bench/src/matrix_vector_bench.cc -o bin/matrix_vector_bench 2> /dev/null
+g++ $COMPILATION_FLAGS $INCLUDE_FLAGS -DLIBLINALG_BACKEND_$1 bench/src/matrix_vector_bench.cc -o bin/matrix_vector_bench 2> /dev/null
 ./bin/matrix_vector_bench
