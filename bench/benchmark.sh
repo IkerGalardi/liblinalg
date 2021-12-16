5f677ec80c85ed5280a599461aba4fbbf0e0a427
IkerGalardi <contacto.ikergalardi@gmail.com>
2021-12-16 09:55:47 +0100

    Reorganized code and got source files

--- bench/benchmark.sh
+++ bench/benchmark.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-if [ $1 = "help" ] || [ -z $1 ]                         
+if [ $1 = "help" ] || [ -z $1 ]
 then
     echo "A backend needs to be selected to test."
-    echo "Available options are SIMD_SSE and SIMPLE_CPU."
-    exit                                                  
+    echo "Available options are SIMD_SSE and SIMPLE_CPU"
+    exit
 fi
 
 COMPILATION_FLAGS="-msse4.1 -mavx2 -m64 -std=c++17"
@@ -23,6 +23,8 @@ g++ $COMPILATION_FLAGS $INCLUDE_FLAGS -DLIBLINALG_BACKEND_SIMPLE_CPU bench/src/m
 # Vector matrix benchmark
 g++ $COMPILATION_FLAGS $INCLUDE_FLAGS -DLIBLINALG_BACKEND_SIMPLE_CPU bench/src/matrix_vector_bench.cc -o bin/matrix_vector_bench 2> /dev/null
 ./bin/matrix_vector_bench
+
+rm -f bin/vector_bench bin/matrix_bench matrix_vector_bench
 
 echo "- $1 results:"
 
---
+++ bench/src/vector_bench.cc
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <chrono>
+#include <vector>
+
+#include "liblinalg.hh"
+
+int main(int argc, char** argv) {
+    constexpr uint64_t vector_size = 100000;
+
+    vecf<vector_size> a = {2, };
+    vecf<vector_size> b = {1, };
+
+    {
+        std::cout << "vecf<" << vector_size << "> addition... " << std::flush;
+        auto start_time = std::chrono::steady_clock::now();
+        auto res = a + b;
+        (void)res;
+        auto end_time = std::chrono::steady_clock::now();
+        std::cout << "Took " << std::chrono::duration_cast<std::chrono::nanoseconds>(end_time - start_time).count()
+                             << "ns" << std::endl;
+    }
+
+    {
+        std::cout << "vecf<" << vector_size << "> dot... " << std::flush;
+        auto start_time = std::chrono::steady_clock::now();
+        auto res = dot(a, b);
+        (void)res;
+        auto end_time = std::chrono::steady_clock::now();
+        std::cout << "Took " << std::chrono::duration_cast<std::chrono::nanoseconds>(end_time - start_time).count()
+                             << "ns" << std::endl;
+    }
+
+    {
+        std::cout << "vecf<" << vector_size << "> multiplication... " << std::flush;
+        auto start_time = std::chrono::steady_clock::now();
+        auto res = 7 * b;
+        (void)res;
+        auto end_time = std::chrono::steady_clock::now();
+        std::cout << "Took " << std::chrono::duration_cast<std::chrono::nanoseconds>(end_time - start_time).count()
+                             << "ns" << std::endl;
+    }
+}
