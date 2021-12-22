c4df15126d487c89cfbce24325a3d5ae7b058f18
IkerGalardi <contacto.ikergalardi@gmail.com>
2021-12-22 09:24:56 +0100

    Cleaner cpp generation and code

--- generate_operations.py
+++ generate_operations.py
@@ -3,101 +3,206 @@
 import numpy as np
 import random
 
+# Settings to generate the tests
 OPERATIONS_PER_SIZE = 1
 SIZES_TO_TEST = [2, 8, 16, 22, 32]
 
-def generate_vector_elements_string(size):
-    return vector_string_from_np_vector(np.random.randint(0, 9, size))
+#
+# Generates a random vector of integers.
+#
+def generate_vector_elements(size):
+    return np.random.randint(0, 9, size)
 
-def vector_string_from_np_vector(vector):
+#
+# Returns the string that represents that vector in form of cpp array 
+# creation: '{e1, e2, e3, ...}'
+#
+def vector_string_from_array(vector):
     elements_string = np.array2string(vector, separator=',')
     elements_string = elements_string.replace('[', '{')
     elements_string = elements_string.replace(']', '}')
 
     return elements_string
 
-def vectordef_string_from_np_vector(name, vector):
+#
+# Creates a definition string of cpp given a name and a vector.
+#
+def vector_definition_from_array(name, vector):
     vector_string = "vecf<{}> {}= ".format(len(vector), name)
-    elems = vector_string_from_np_vector(vector)
+    elems = vector_string_from_array(vector)
     vector_string = vector_string + elems + ';'
     return vector_string
 
-def random_vector_string(size):
-    vector_string = "vecf<{}> = ".format(size)
+def print_vector_comparison_tests():
+    print('void test_vector_comparisons() {')
+    var_num = 0
+    for i in SIZES_TO_TEST:
+        for j in range(OPERATIONS_PER_SIZE):
+            a1_name = "a{}".format(var_num)
+            b1_name = "b{}".format(var_num)
 
-    elements_string = generate_vector_elements_string(size)
+            a = generate_vector_elements(i)
+            b = a
 
-    vector_string = vector_string + elements_string + ';'
+            print('    ' + vector_definition_from_array(a1_name, a))
+            print('    ' + vector_definition_from_array(b1_name, b))
+            print('    assert((a{} == b{}));'.format(var_num, var_num));
+            
+            var_num = var_num + 1
 
-    return vector_string
+    print('}')
 
-# Generate vector operations
-var_num = 0
-for i in SIZES_TO_TEST:
-    # Addition operations
+
+def print_vector_deep_copies_tests():
+    print('// TODO: vector deep copies test')
+
+def print_vector_addition_tests():
     print('void test_vector_addition() {')
-    for j in range(OPERATIONS_PER_SIZE):
-        v1 = np.random.randint(0, 9, i)
-        v2 = np.random.randint(0, 9, i)
-        res = v1 + v2
-
-        print('    ' + vectordef_string_from_np_vector('a{}'.format(var_num), v1))
-        print('    ' + vectordef_string_from_np_vector('b{}'.format(var_num), v2))
-        print('    ' + 'auto r{} = a{} + b{};'.format(var_num, var_num, var_num))
-        print('    ' + vectordef_string_from_np_vector('e{}'.format(var_num), res))
-        print('    ' + 'assert(r{} == e{});'.format(var_num, var_num))
-
-        var_num = var_num + 1
-    print('}')
     var_num = 0
+    for i in SIZES_TO_TEST:
+        for j in range(OPERATIONS_PER_SIZE):
+            a1_name = "a{}".format(var_num)
+            b1_name = "b{}".format(var_num)
+            e1_name = "e{}".format(var_num)
 
-    # Substraction operations
+            a = generate_vector_elements(i)
+            b = generate_vector_elements(i)
+            e = a + b
+
+            print('    ' + vector_definition_from_array(a1_name, a))
+            print('    ' + vector_definition_from_array(b1_name, b))
+            print('    ' + vector_definition_from_array(e1_name, e))
+            print('    auto r{} = a{} + b{};'.format(var_num, var_num, var_num))
+            print('    assert((r{} == e{}));'.format(var_num, var_num));
+            
+            var_num = var_num + 1
+
+    print('}')
+
+def print_vector_substraction_tests():
     print('void test_vector_substraction() {')
-    for j in range(OPERATIONS_PER_SIZE):
-        v1 = np.random.randint(0, 9, i)
-        v2 = np.random.randint(0, 9, i)
-        res = v1 - v2
-
-        print('    ' + vectordef_string_from_np_vector('a{}'.format(var_num), v1))
-        print('    ' + vectordef_string_from_np_vector('b{}'.format(var_num), v2))
-        print('    ' + 'auto r{} = a{} - b{};'.format(var_num, var_num, var_num))
-        print('    ' + vectordef_string_from_np_vector('e{}'.format(var_num), res))
-        print('    ' + 'assert(r{} == e{});'.format(var_num, var_num))
-
-        var_num = var_num + 1
-    print('}')
     var_num = 0
+    for i in SIZES_TO_TEST:
+        for j in range(OPERATIONS_PER_SIZE):
+            a1_name = "a{}".format(var_num)
+            b1_name = "b{}".format(var_num)
+            e1_name = "e{}".format(var_num)
 
-    print('void test_vector_dot() {')
-    for j in range(OPERATIONS_PER_SIZE):
-        v1 = np.random.randint(0, 9, i)
-        v2 = np.random.randint(0, 9, i)
-        res = np.dot(v1, v2)
+            a = generate_vector_elements(i)
+            b = generate_vector_elements(i)
+            e = a - b
 
-        print('    ' + vectordef_string_from_np_vector('a{}'.format(var_num), v1))
-        print('    ' + vectordef_string_from_np_vector('b{}'.format(var_num), v2))
-        print('    ' + 'auto r{} = dot(a{}, b{});'.format(var_num, var_num, var_num))
-        print('    ' + 'float e{} = {}'.format(var_num, res))
-        print('    ' + 'assert(r{} == e{});'.format(var_num, var_num))
+            print('    ' + vector_definition_from_array(a1_name, a))
+            print('    ' + vector_definition_from_array(b1_name, b))
+            print('    ' + vector_definition_from_array(e1_name, e))
+            print('    auto r{} = a{} - b{};'.format(var_num, var_num, var_num))
+            print('    assert((r{} == e{}));'.format(var_num, var_num));
+            
+            var_num = var_num + 1
 
-        var_num = var_num + 1
-    var_num = 0
     print('}')
 
+def print_vector_length_tests():
     print('void test_vector_length() {')
-    for j in range(OPERATIONS_PER_SIZE):
-        v1 = np.random.randint(0, 9, i)
-        v2 = np.random.randint(0, 9, i)
-        res = v1 - v2
-
-        print('    ' + vectordef_string_from_np_vector('a{}'.format(var_num), v1))
-        print('    ' + vectordef_string_from_np_vector('b{}'.format(var_num), v2))
-        print('    ' + 'auto r{} = dot(a{}, b{});'.format(var_num, var_num, var_num))
-        print('    ' + vectordef_string_from_np_vector('e{}'.format(var_num), res))
-        print('    ' + 'assert(r{} == e{});'.format(var_num, var_num))
-        print('')
-
-        var_num = var_num + 1
     var_num = 0
+    for i in SIZES_TO_TEST:
+        for j in range(OPERATIONS_PER_SIZE):
+            a1_name = "a{}".format(var_num)
+
+            a = generate_vector_elements(i)
+            e = sum(i**2 for i in a)
+
+            print('    ' + vector_definition_from_array(a1_name, a))
+            print('    float e{} = {};'.format(var_num, e))
+            print('    auto r{} = length_squared(a{});'.format(var_num, var_num))
+            print('    assert((r{} == e{}));'.format(var_num, var_num));
+            
+            var_num = var_num + 1
+
     print('}')
 
+def print_vector_dot_tests():
+    print('void test_vector_dotproduct() {')
+    var_num = 0
+    for i in SIZES_TO_TEST:
+        for j in range(OPERATIONS_PER_SIZE):
+            a1_name = "a{}".format(var_num)
+            b1_name = "b{}".format(var_num)
+            e1_name = "e{}".format(var_num)
+
+            a = generate_vector_elements(i)
+            b = generate_vector_elements(i)
+            e = np.dot(a, b)
+
+            print('    ' + vector_definition_from_array(a1_name, a))
+            print('    ' + vector_definition_from_array(b1_name, b))
+            print('    float e{} = {};'.format(var_num, e))
+            print('    auto r{} = dot(a{}, b{});'.format(var_num, var_num, var_num))
+            print('    assert((r{} == e{}));'.format(var_num, var_num));
+            
+            var_num = var_num + 1
+
+    print('}')
+
+
+def print_vector_multiplication_tests():
+    print('void test_vector_multiplication() {')
+    var_num = 0
+    for i in SIZES_TO_TEST:
+        for j in range(OPERATIONS_PER_SIZE):
+            a1_name = "a{}".format(var_num)
+            b1_name = "b{}".format(var_num)
+            e1_name = "e{}".format(var_num)
+
+            a = generate_vector_elements(i)
+            b = generate_vector_elements(i)
+            e = np.multiply(a, b)
+
+            print('    ' + vector_definition_from_array(a1_name, a))
+            print('    ' + vector_definition_from_array(b1_name, b))
+            print('    ' + vector_definition_from_array(e1_name, e))
+            print('    auto r{} = a{} * b{};'.format(var_num, var_num, var_num))
+            print('    assert((r{} == e{}));'.format(var_num, var_num));
+            
+            var_num = var_num + 1
+
+    print('}')
+
+# Printing all the headers and macros
+print('#include <cassert>')
+print('#include <iostream>')
+print('')
+print('#include "liblinalg.hh"')
+print('')
+print('#define EXECUTE_TEST(test_func) \\')
+print('    std::cout << "Executing " #test_func "... " << std::flush; \\')
+print('    test_func(); \\')
+print('    std::cout << "OK" << std::endl')
+print('')
+
+# Printing all the tests
+print_vector_comparison_tests()
+print_vector_deep_copies_tests()
+print_vector_addition_tests()
+print_vector_substraction_tests()
+print_vector_length_tests()
+print_vector_dot_tests()
+print_vector_multiplication_tests()
+
+# Printing the main function with all the tests
+print('')
+print('int main() {')
+print('    EXECUTE_TEST(test_vector_comparisons);')
+#print('    EXECUTE_TEST(test_vector_deep_copies);')
+print('    EXECUTE_TEST(test_vector_addition);')
+print('    EXECUTE_TEST(test_vector_substraction);')
+print('    EXECUTE_TEST(test_vector_length);')
+print('    EXECUTE_TEST(test_vector_dotproduct);')
+print('    EXECUTE_TEST(test_vector_multiplication);')
+#print('    EXECUTE_TEST(test_matrix_indexing);')
+#print('    EXECUTE_TEST(test_matrix_deep_copies);')
+#print('    EXECUTE_TEST(test_matrix_comparisons);')
+#print('    EXECUTE_TEST(test_matrix_addition);')
+#print('    EXECUTE_TEST(test_matrix_multiplication);')
+#print('    EXECUTE_TEST(test_matrix_vector_multiplication);')
+print('}')
